634d61179d69ef6e38026a0fc0538110b6d47bb6
William Bojczuk <williambojczuk@gmail.com>
2023-02-27 18:52:17 -0800

    Initial COmmit

--- js/anims.js
+++ js/anims.js
@@ -8,17 +8,23 @@ const animsObj = {
         }
     },
     animHome: ()=>{
-        const tl = gsap.timeline({defaults:{duration: 1.5, ease: "power4.out"}});
-        tl.to("header h1", {
-            "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
-            y:0,
-            opacity:1
-        })
-        .to("header h2", {
-            opacity:1,
-            "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
-            "transform":"translateY(0)"
-        }, "-=.8")
+        const bgIMG = new Image();
+        bgIMG.onload = ()=>{
+            document.getElementsByTagName("header")[0].style.backgroundImage = "url('./img/mainbg.jpg')";
+            const tl = gsap.timeline({defaults:{duration: 1.5, ease: "power4.out"}});
+            tl.to("header h1", {
+                "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+                y:0,
+                opacity:1
+            })
+            .to("header h2", {
+                opacity:1,
+                "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+                "transform":"translateY(0)"
+            }, "-=.8")
+        }
+        bgIMG.src = "../img/mainbg.jpg";
+        
     }
 }
 animsObj.init();
